6387420cd5e503f037bdf5699dc850ae3fc4b78b
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2025-01-16 22:46:13 +0700

    feat: Add navigation logic to switch between Home, Menu, and Contact sections
    - Import `loadHome`, `loadMenu`, and `loadContact` to dynamically render respective sections
    - Add event listener to the navigation bar for seamless tab switching based on user clicks
    - Implement logic to show and hide content containers (`home-container`, `menu-container`, `contact-container`) using the `hidden` class
    - Ensure dynamic section visibility for improved user experience and modular design

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+import "./styles.css";
 import loadHome from "./home.js";
+import loadMenu from "./menu.js";
+import loadContact from "./contact.js";
 
 loadHome();
+loadMenu();
+loadContact();
+
+const homeContainer = document.querySelector(".home-container");
+const menuContainer = document.querySelector(".menu-container");
+const contactContainer = document.querySelector(".contact-container");
+
+const nav = document.querySelector("nav");
+nav.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target.classList.contains("home")) {
+        menuContainer.classList.add("hidden");
+        contactContainer.classList.add("hidden");
+        homeContainer.classList.remove("hidden");
+    }
+    else if (target.classList.contains("menu")) {
+        homeContainer.classList.add("hidden");
+        contactContainer.classList.add("hidden");
+        menuContainer.classList.remove("hidden");
+    }
+    else if (target.classList.contains("contact")) {
+        homeContainer.classList.add("hidden");
+        menuContainer.classList.add("hidden");
+        contactContainer.classList.remove("hidden");
+    }
+});
