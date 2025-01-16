6fa8a3ddacfefcd951e3dfd872607d6bf032a720
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2025-01-16 22:25:09 +0700

    feat: Add contact.js to dynamically render contact information
    - Create `loadContact` function to dynamically generate and display the contact section
    - Include phone, email, and address information with corresponding icons
    - Organize contact details in a structured and styled container (`contact-container`)
    - Enhance maintainability and modularity by dynamically rendering contact content

---
+++ src/contact.js
@@ -0,0 +1,40 @@
+import phone from "./images/contacts/phone.svg";
+import email from "./images/contacts/email.svg";
+import address from "./images/contacts/address.svg";
+
+export default function loadContact() {
+    const content = document.querySelector("#content");
+    const contactContainer = document.createElement("div");
+
+    contactContainer.classList.toggle("contact-container");
+    contactContainer.classList.toggle("hidden");
+
+    const contactItemContainer1 = document.createElement("div");
+    const phoneIcon = document.createElement("img");
+    phoneIcon.src = phone;
+    const phoneNumber = document.createElement("p");
+    phoneNumber.textContent = "(555) 123-4567";
+    contactItemContainer1.appendChild(phoneIcon);
+    contactItemContainer1.appendChild(phoneNumber);
+    contactContainer.appendChild(contactItemContainer1);
+
+    const contactItemContainer2 = document.createElement("div");
+    const emailIcon = document.createElement("img");
+    emailIcon.src = email;
+    const emailAddress = document.createElement("p");
+    emailAddress.textContent = "info@coastalgrill.com";
+    contactItemContainer2.appendChild(emailIcon);
+    contactItemContainer2.appendChild(emailAddress);
+    contactContainer.appendChild(contactItemContainer2);
+
+    const contactItemContainer3 = document.createElement("div");
+    const addressIcon = document.createElement("img");
+    addressIcon.src = address;
+    const addressDetail = document.createElement("p");
+    addressDetail.textContent = "123 Ocean View Blvd, Seaside City, CA 90210";
+    contactItemContainer3.appendChild(addressIcon);
+    contactItemContainer3.appendChild(addressDetail);
+    contactContainer.appendChild(contactItemContainer3);
+
+    content.appendChild(contactContainer);
+}
