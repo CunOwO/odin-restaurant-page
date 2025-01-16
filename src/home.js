6aacd809047533d22b1b4ade783220eb6d0d41cc
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2025-01-16 22:59:58 +0700

    style: Add base styles and normalize CSS for consistent design
    - Import `modern-normalize.css` for cross-browser style consistency
    - Add a `hidden` utility class to toggle visibility of elements using `display: none`

--- src/home.js
+++ src/home.js
@@ -3,8 +3,6 @@ export default function loadHome() {
     const content = document.querySelector("#content");
     const homeContainer = document.createElement("div");
     homeContainer.classList.toggle("home-container");
-    const headline = document.createElement("h1");
-    headline.textContent = "Coastal Grill";
 
     const image = document.createElement("img");
     image.src = coverImage;
@@ -12,7 +10,6 @@ export default function loadHome() {
     const description = document.createElement("p");
     description.textContent = "Enjoy a unique dining experience with stunning ocean views. Coastal Grill offers fresh seafood and handcrafted cocktails, perfect for any occasion."
 
-    homeContainer.appendChild(headline);
     homeContainer.appendChild(image);
     homeContainer.appendChild(description);
     content.appendChild(homeContainer);
