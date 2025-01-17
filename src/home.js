1dd4b65b27d6fc2b257c2b0ea277361c22a9431a
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2025-01-17 14:19:31 +0700

    refactor: Simplify and enhance home.js content structure

--- src/home.js
+++ src/home.js
@@ -1,16 +1,15 @@
-import coverImage from "./images/cover-image.jpg";
 export default function loadHome() {
     const content = document.querySelector("#content");
     const homeContainer = document.createElement("div");
     homeContainer.classList.toggle("home-container");
 
-    const image = document.createElement("img");
-    image.src = coverImage;
+    const hero = document.createElement("h1");
+    hero.textContent = "Welcome";
 
     const description = document.createElement("p");
     description.textContent = "Enjoy a unique dining experience with stunning ocean views. Coastal Grill offers fresh seafood and handcrafted cocktails, perfect for any occasion."
 
-    homeContainer.appendChild(image);
+    homeContainer.appendChild(hero);
     homeContainer.appendChild(description);
     content.appendChild(homeContainer);
 }
